7ba0d8476b1bf6f8d11b2dbce74cd032adef707a
miguelvalden <miguelvalden77@gmail.com>
2022-11-08 15:23:17 +0100

    ejercicios

--- javascript.js
+++ javascript.js
@@ -1,9 +1,50 @@
 
+// map --> Siempre devuelve un array
 
+let numbers = [2, 45, 7, 8, 34, 21, 43, 22]
+
+let newArr = numbers.map((num)=>{
+    return num + 20
+})
+
+console.log(newArr)
+
+let coches = [
+    {
+        marca: "Hyundai",
+        precio: 14000
+    },
+    {
+        marca: "Audi",
+        precio: 21000
+    },
+    {
+        marca: "Daewo",
+        precio: 12000
+    },
+    {
+        marca: "Bugatti",
+        precio: 300000
+    }
+]
+
+let cochesArr = coches.forEach((e)=>{
+    console.log(e.precio * 0.9)
+})
+
+console.log(coches)
+console.log(cochesArr)
+
+let nuevoArr = coches.map((e)=>{
+    return e.marca
+})
+
+console.log(nuevoArr)
 
 // map
     // 1. Haz una función que tome como parámetro un array de strings y devuelva ese array de strings pasados a mayúsculas.
 
+    
 
     // 2. Haz una función que reciba como parámetro un array de números y devuelva un array de esos números pero multiplicados por 4 si son menores de 10 y multiplicados por 2 si son mayores de 10 o iguales
 
@@ -39,6 +80,6 @@
 
 
     // forEach
-        // 2. dado el mismo array de objetos que antes, se debe hacer console.log() de cada nombre pasado a mayúscula
+        // 2. dado el mismo array de objetos que antes, se debe hacer console.log() de cada nombre pasado a mayúscula. No es una función
 
         
